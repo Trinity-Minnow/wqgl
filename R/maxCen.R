ee4fce2c0721a7a3d50b843ad12552dadf8794a4
Jeff Row <jeff.row@me.com>
2022-11-16 14:59:40 -0500

    Add censored summary stats.

---
+++ R/maxCen.R
@@ -0,0 +1,23 @@
+#' Calculate Max Value for Censored Data.
+#'
+#' @description Calculate and format max value for censored data.  Takes highest detected value. If all values are censored it takes the smalled LRL.
+#' @param vals Character vector with potential censored values.
+#' @param format Whether to format the output to the desired number of digits.
+#' @param digits Number of significant digits.
+#'
+#' @return Minimum value
+#' @export
+#'
+#' @examples
+
+maxCen<-function(vals,format=F,digits=3) {
+  if(all(grepl("<",na.omit(vals))==T)){
+    maxval<-paste0("<",subdl(min(vals,na.rm=T)))
+  } else{
+    #maxval=max(subdl(vals),na.rm=T)
+    maxval=max(vals[!grepl("<",vals)])
+
+  }
+  if(format==T){maxval=forcenval2(maxval,digits)} else{maxval=as.character(maxval)}
+  return(maxval)
+}
---
+++ R/minCen.R
@@ -0,0 +1,21 @@
+#' Calculate Min Value for Censored Data.
+#'
+#' @description Calculate and format min value for censored data.  Retains "<" if smallest value is censored.
+#' @param vals Character vector with potential censored values
+#' @param format Whether to format the output to the desired number of digits
+#' @param digits Number of significant digits
+#'
+#' @return Minimum value
+#' @export
+#'
+#' @examples
+
+minCen<-function(vals,format=F,digits=3) {
+  df1<-data.frame(vals) %>%
+    mutate(val2=subdl(vals))
+
+  minval<-df1[which(df1$val2==min(df1$val2,na.rm=T)),"vals"][1]
+
+  if(format==T){minval=forcenval2(minval,digits)} else{minval=as.character(minval)}
+  return(minval)
+}
